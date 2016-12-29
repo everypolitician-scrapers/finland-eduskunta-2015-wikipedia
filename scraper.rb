c54772fa5d832d375260444f2430d02f5e80a000
Tony Bowden <tony@tmtm.com>
2016-12-29 16:22:03 +0000

    Add replacements
    The final column sometimes has information on a replacement.
    
    Based on layout at https://fi.wikipedia.org/w/index.php?title=Luettelo_vaalikauden_2015%E2%80%932019_kansanedustajista&oldid=15930540

--- scraper.rb
+++ scraper.rb
@@ -60,6 +60,24 @@ class MemberRow < Scraped::HTML
     37
   end
 
+  field :end_date do
+    return unless tds[6].text.include? 'seuraajaksi'
+    Date.new(*tds[6].text[/(\d+\.\d+.\d{4})/].split('.').reverse.map(&:to_i)).to_s
+  end
+
+  field :replacement do
+    return unless end_date
+    who = tds[6].at_css('a')
+    {
+      name:         who.text.tidy,
+      party_id:     party_id,
+      constituency: constituency,
+      wikiname:     who.attr('title'),
+      term:         37,
+      start_date:   end_date,
+    }
+  end
+
   private
 
   def tds
@@ -69,8 +87,15 @@ end
 
 url = 'https://fi.wikipedia.org/wiki/Luettelo_vaalikauden_2015%E2%80%932019_kansanedustajista'
 page = MembersPage.new(response: Scraped::Request.new(url: url).response)
-data = page.members.map(&:to_h)
-# puts data
+data = page.members.map do |mem|
+  md = mem.to_h.reject { |_, v| v.to_s.empty? }
+  if replacement = md.delete(:replacement)
+    [md, replacement]
+  else
+    md
+  end
+end.flatten
 
+# puts data
 ScraperWiki.sqliteexecute('DELETE FROM data') rescue nil
 ScraperWiki.save_sqlite(%i(name party_id term), data)
